3443ff7382b23bdeb1023c16d499086e463ac40a
zaarheed <zahid@zmdev.com>
2020-04-14 13:18:03 +0100

    fix grid on mobile

--- src/components/RecipesList.vue
+++ src/components/RecipesList.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="grid grid-cols-3 gap-3 text-black">
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-black">
 		<RecipeCard
 			v-for="recipe of recipes"
 			:key="recipe.id"
