462ffe4953e51d9ef9d46d14e6c79ccb9d035ca7
zaarheed <zahid@zmdev.com>
2020-04-14 01:50:44 +0100

    moved button in to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,16 @@
 <template>
-	<header class="bg-dark header">
+	<header class="bg-dark bg-image header">
 		<img src="../assets/logo.png" class="w-64 m-auto mb-4">
 		<h1 class="font-sans text-3xl text-center text-white mb-2">Lockdown Recipes</h1>
 
 		<AddIngredient />
 		<SelectedIngredients />
+
+		<section class="flex px-3 text-white justify-end py-2">
+			<button class="bg-transparent hover:bg-transparent text-white bg-dark font-semibold py-2 px-4 border border-gray-400 rounded shadow" v-on:click="getRecipes">
+				Get Recipes!
+			</button>
+		</section>
 
 	</header>
 </template>
@@ -29,6 +35,9 @@ header {
 
 .bg-dark {
 	background: #13293D;
+}
+
+.bg-image {
 	background-image: url("../assets/header-bg.png");
 	background-position: center;
 	background-size: cover;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,5 @@
 <template>
 	<main>
-		<section class="flex px-3 bg-dark text-white justify-end py-2">
-			<button class="bg-transparent hover:bg-transparent text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow" v-on:click="getRecipes">
-				Get Recipes!
-			</button>
-		</section>
-
 		<section class="px-3 mb-1 py-2">
 			<RecipesList />
 		</section>
